6dbf868d4483e0bc4243246bad865fafe3fd912e
ZayanKhan <khanzayanakbar@gmail.com>
2026-09-15 18:06:32 +0500

    Added local storage
    Refactor todo list code for better readability and maintainability.

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 const input = document.querySelector("#todo-input");
 const ul = document.querySelector("#todo-list");
 
-const todos = [];
+// Local Storage se todos lena
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 function addTodo(event) {
   event.preventDefault();
 
-   if (input.value === "") {
+  if (input.value === "") {
     alert("Enter Something");
     return;
   }
 
   todos.push(input.value);
 
+  // Local Storage mein save karna
+  localStorage.setItem("todos", JSON.stringify(todos));
+
   showTodos();
+
+  input.value = "";
 }
 
 function showTodos() {
   ul.innerHTML = "";
 
   for (let i = 0; i < todos.length; i++) {
-   ul.innerHTML += `<li>${todos[i]} <button class="delete" onclick="deleteTodo(${i})">delete</button></li>`;
+    ul.innerHTML += `
+      <li>
+        ${todos[i]}
+        <button class="delete" onclick="deleteTodo(${i})">
+          delete
+        </button>
+      </li>
+    `;
   }
 }
 
@@ -28,6 +41,9 @@ showTodos();
 
 function deleteTodo(index) {
   todos.splice(index, 1);
+
+  // Delete ke baad Local Storage update karna
+  localStorage.setItem("todos", JSON.stringify(todos));
 
   showTodos();
 }
